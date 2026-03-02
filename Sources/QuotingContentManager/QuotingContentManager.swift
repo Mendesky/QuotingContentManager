6770e48958c58b290964f17b04478c50bd185c4c
Grady Zhuo <gradyzhuo@jwcpas.com.tw>
2026-03-02 14:28:41 +0800

    Move fetch* functions from ContractNoteManager to QuotingContentManager

--- Sources/QuotingContentManager/QuotingContentManager.swift
+++ Sources/QuotingContentManager/QuotingContentManager.swift
@@ -38,4 +38,51 @@ public struct QuotingContentManager: Sendable {
     public func getNote(uniqueCode: String) -> ContractNoteManager.ContractNoteInfo? {
         contractNoteManager.notes.filter { !$0.deprecated }.first { $0.uniqueCode == uniqueCode }
     }
+
+    public func fetchNotes(tip: String) -> [ContractNoteManager.ContractNoteInfo] {
+        fetchNotes(subsetOf: ["Tip/\(tip)"])
+    }
+
+    public func fetchNotes(mutexTags: [String]) -> [ContractNoteManager.ContractNoteInfo] {
+        contractNoteManager.notes.filter {
+            if case let .tags(tags) = $0.mutex {
+                Set<String>(mutexTags).isSubset(of: Set<String>(tags))
+            } else {
+                false
+            }
+        }
+    }
+
+    public func fetchNotes(serviceItem: String) -> [ContractNoteManager.ContractNoteInfo] {
+        fetchNotes(subsetOf: ["ServiceItem/\(serviceItem)"])
+    }
+
+    public func fetchNotes(serviceItemConfig key: String, value: String) -> [ContractNoteManager.ContractNoteInfo] {
+        fetchNotes(subsetOf: ["ServiceItemConfig/\(key):\(value)"])
+    }
+
+    public func fetchNotes(subsetOf tags: [String]) -> [ContractNoteManager.ContractNoteInfo] {
+        contractNoteManager.notes.filter { note in
+            note.isSubsetOf(tags: tags)
+        }.sorted { lhs, rhs in
+            lhs.weight > rhs.weight
+        }
+    }
+
+    public func fetchNotes(symmetricDifference tags: [String]) -> [ContractNoteManager.ContractNoteInfo] {
+        contractNoteManager.notes.filter { note in
+            !note.isSubsetOf(tags: tags)
+        }.sorted { lhs, rhs in
+            lhs.weight > rhs.weight
+        }
+    }
+
+    public func findIndexToInsert(willInsert noteInfo: ContractNoteManager.ContractNoteInfo, inUniqueCodes uniqueCodes: [String?]) -> Int? {
+        uniqueCodes.firstIndex(where: { uniqueCode in
+            guard let currentNoteInfo = contractNoteManager.notes.first(where: { $0.uniqueCode == uniqueCode }) else {
+                return false
+            }
+            return currentNoteInfo.weight < noteInfo.weight
+        })
+    }
 }
